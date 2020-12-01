08d40e4537a7d0394656d334a28fe966e2acd0b7
Alexey Maltsev <axel.user.perm@gmail.com>
2020-12-01 22:17:29 +0300

    Added solution for part 1 of day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = args[0];
+
+            var inputs = File.ReadAllLines(fileName).Select(int.Parse).ToArray();
+
+            var (first, second) = FindPair(inputs);
+            Console.WriteLine($"Result is {first} * {second} = {first * second}");
+        }
+
+        private static (int first, int second) FindPair(int[] values)
+        {
+            var complementaryValues = new Dictionary<int, int>();
+
+            foreach (var value in values)
+            {
+                complementaryValues[2020 - value] = value;
+            }
+            
+            foreach (var value in values)
+            {
+                if (complementaryValues.TryGetValue(value, out var complementaryValue))
+                {
+                    return (value, complementaryValue);
+                }
+            }
+
+            throw new Exception("Could not find pair which sum is equal 2020");
+        }
+    }
+}
