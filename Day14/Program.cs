f9613b92a888cea8915a0e803d5151e2cc0fe285
Alexey Maltsev <axel.user.perm@gmail.com>
2020-12-15 00:24:39 +0300

    Minor refactoring

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -44,17 +44,11 @@ namespace Day14
                     var address = Convert.ToString(long.Parse(match.Groups["address"].Value), 2)
                         .PadLeft(36, '0').ToArray();
                     var value = long.Parse(match.Groups["value"].Value);
-                    var maskedAddress = mask.Zip(address, (m, a) =>
+                    var maskedAddress = mask.Zip(address, (m, a) => m switch
                     {
-                        switch (m)
-                        {
-                            case '0':
-                                return a;
-                            case 'X':
-                                return 'X';
-                            default:
-                                return m;
-                        }
+                        '0' => a,
+                        'X' => 'X',
+                        _ => m
                     }).ToArray();
 
                     foreach (var affectedAddress in GetAddresses(maskedAddress))
@@ -65,64 +59,61 @@ namespace Day14
             }
 
             return memory.Values.Sum();
+        }
 
-            IEnumerable<long> GetAddresses(char[] mask)
+        private static IEnumerable<long> GetAddresses(char[] mask)
+        {
+            // we have bitmask with 0, 1 and X
+            // when we met X at position i, we invoke function t(i)
+            // t(i) will add 2 new bitmasks to the list, with bits 1 and 0 at position i
+            // if list already contain masks, then we override each mask with 1 and 0 bits at position i
+            // when we finish processing original mask we should have addresses without any X
+            // if we didn't met X, then we just add original mask, which is a valid address, to the list
+            // e.g:
+            // mask: 000000000000000000000000000000X1101X
+            // at position 30 we met first X
+            // we add masks: 000000000000000000000000000000(1)1101X
+            //               000000000000000000000000000000(0)1101X
+            // at position 35 we met second X
+            // we override each mask with two, passing 1 and 0 at position 35
+            // first one we override with: 000000000000000000000000000000(1)1101(1)
+            //                             000000000000000000000000000000(1)1101(0)
+            // second one we override with: 000000000000000000000000000000(0)1101(1)
+            //                              000000000000000000000000000000(0)1101(0)
+            // at the end we will have addresses:
+            //    000000000000000000000000000000(1)1101(1)
+            //    000000000000000000000000000000(1)1101(0)
+            //    000000000000000000000000000000(0)1101(1)
+            //    000000000000000000000000000000(0)1101(0)
+
+            var addresses = new List<char[]> {mask};
+
+            for (var bitIdx = 0; bitIdx < mask.Length; bitIdx++)
             {
-                // we have bitmask with 0, 1 and X
-                // when we met X at position i, we invoke function t(i)
-                // t(i) will add 2 new bitmasks to the list, with bits 1 and 0 at position i
-                // if list already contain masks, then we override each mask with 1 and 0 bits at position i
-                // when we finish processing original mask we should have addresses without any X
-                // if we didn't met X, then we just add original mask, which is a valid address, to the list
-                // e.g:
-                // mask: 000000000000000000000000000000X1101X
-                // at position 30 we met first X
-                // we add masks: 000000000000000000000000000000(1)1101X
-                //               000000000000000000000000000000(0)1101X
-                // at position 35 we met second X
-                // we override each mask with two, passing 1 and 0 at position 35
-                // first one we override with: 000000000000000000000000000000(1)1101(1)
-                //                             000000000000000000000000000000(1)1101(0)
-                // second one we override with: 000000000000000000000000000000(0)1101(1)
-                //                              000000000000000000000000000000(0)1101(0)
-                // at the end we will have addresses:
-                //    000000000000000000000000000000(1)1101(1)
-                //    000000000000000000000000000000(1)1101(0)
-                //    000000000000000000000000000000(0)1101(1)
-                //    000000000000000000000000000000(0)1101(0)
-                
-                var addresses = new List<char[]>
+                switch (mask[bitIdx])
                 {
-                    mask
-                };
-
-                for (var bitIdx = 0; bitIdx < mask.Length; bitIdx++)
-                {
-                    switch (mask[bitIdx])
-                    {
-                        case '0':
-                        case '1':
-                            continue;
-                        case 'X':
-                            var newAddresses = new List<char[]>();
-                            foreach (var address in addresses)
+                    case '0':
+                    case '1':
+                        continue;
+                    case 'X':
+                        var newAddresses = new List<char[]>();
+                        foreach (var address in addresses)
+                        {
+                            foreach (var bit in new[] {'0', '1'})
                             {
-                                foreach (var bit in new [] {'0', '1'})
-                                {
-                                    var newAddress = new char[address.Length];
-                                    address.CopyTo(newAddress, 0);
-                                    newAddress[bitIdx] = bit;
-                                    newAddresses.Add(newAddress);
-                                }
+                                var newAddress = new char[address.Length];
+                                address.CopyTo(newAddress, 0);
+                                newAddress[bitIdx] = bit;
+                                newAddresses.Add(newAddress);
                             }
+                        }
 
-                            addresses = newAddresses;
-                            break;
-                    }
+                        addresses = newAddresses;
+                        break;
                 }
-
-                return addresses.Select(a => Convert.ToInt64(new string(a), 2));
             }
+
+            return addresses.Select(a => Convert.ToInt64(new string(a), 2));
         }
 
         private static long SolvePart1(IEnumerable<string> lines)
