edbd23018a6c3263b54fc026bc30d45630b20e61
Alexey Maltsev <axel.user.perm@gmail.com>
2020-12-17 10:52:06 +0300

    Added solution of second part for day 17

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,43 +7,42 @@ namespace Day17
 {
     class Program
     {
-        private record Cube(int X, int Y, int Z);
-        
+        private record Cube(int X, int Y, int Z = 0, int W = 0);
+
         static void Main(string[] args)
         {
             var fileName = args[0];
             var part = int.Parse(args[1]);
-            
-            switch (part)
+
+            Func<Cube, IEnumerable<Cube>> gerNeighbors = part switch
             {
-                case 1:
-                    Console.WriteLine($"Step 1: {SolvePart1(File.ReadAllLines(fileName))}");
-                    return;
-                case 2:
-                    return;
-            } 
+                1 => GetNeighborsIn3D,
+                2 => GetNeighborsIn4D
+            };
+            
+            Console.WriteLine($"Part {part}: {CountActive(File.ReadAllLines(fileName), gerNeighbors)}");
         }
 
-        private static int SolvePart1(string[] map)
+        private static int CountActive(string[] map, Func<Cube, IEnumerable<Cube>> getNeighbors)
         {
             var state = ReadActiveCubes(map);
 
             for (var cycle = 0; cycle < 6; cycle++)
             {
-                state = CalculateNewState(state);
+                state = CalculateNewState(state, getNeighbors);
             }
 
             return state.Count;
         }
 
-        private static HashSet<Cube> CalculateNewState(HashSet<Cube> prev)
+        private static HashSet<Cube> CalculateNewState(HashSet<Cube> prev, Func<Cube, IEnumerable<Cube>> getNeighbors)
         {
             var active = new HashSet<Cube>();
             var processed = new HashSet<Cube>();
 
             foreach (var activeCube in prev)
             {
-                var activeCount = GetNeighbors(activeCube).Count(prev.Contains);
+                var activeCount = getNeighbors(activeCube).Count(prev.Contains);
 
                 if (activeCount == 2 || activeCount == 3)
                     active.Add(activeCube);
@@ -52,11 +50,11 @@ namespace Day17
                 processed.Add(activeCube);
             }
 
-            foreach (var neighbor in prev.SelectMany(GetNeighbors))
+            foreach (var neighbor in prev.SelectMany(getNeighbors))
             {
                 if(processed.Contains(neighbor)) continue;
 
-                if (GetNeighbors(neighbor).Count(prev.Contains) == 3)
+                if (getNeighbors(neighbor).Count(prev.Contains) == 3)
                     active.Add(neighbor);
 
                 processed.Add(neighbor);
@@ -65,7 +63,7 @@ namespace Day17
             return active;
         }
 
-        private static IEnumerable<Cube> GetNeighbors(Cube current)
+        private static IEnumerable<Cube> GetNeighborsIn3D(Cube current)
         {
             for (var y = current.Y - 1; y <= current.Y + 1; y++)
             {
@@ -81,6 +79,26 @@ namespace Day17
                 }                
             }
         }
+        
+        private static IEnumerable<Cube> GetNeighborsIn4D(Cube current)
+        {
+            for (var y = current.Y - 1; y <= current.Y + 1; y++)
+            {
+                for (var x = current.X - 1; x <= current.X + 1; x++)
+                {
+                    for (var z = current.Z - 1; z <= current.Z + 1; z++)
+                    {
+                        for (var w = current.W - 1; w <= current.W + 1; w++)
+                        {
+                            var neighbor = new Cube(x, y, z, w);
+
+                            if (neighbor != current)
+                                yield return neighbor;
+                        }
+                    }
+                }                
+            }
+        }
 
         private static HashSet<Cube> ReadActiveCubes(string[] map)
         {
@@ -91,7 +109,7 @@ namespace Day17
                 for (var x = 0; x < map[y].Length; x++)
                 {
                     if (map[y][x] == '#')
-                        active.Add(new Cube(x, y, 0));
+                        active.Add(new Cube(x, y));
                 }
             }
 
