af40af5b88e07dfa966abcf85a6daa551f15adce
Alexey Maltsev <axel.user.perm@gmail.com>
2020-12-20 01:00:00 +0300

    WIP on solution for part 2 of day 19

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -13,11 +13,70 @@ namespace Day19
             var fileName = args[0];
             var part = int.Parse(args[1]);
 
-            var count = Solve(File.ReadAllLines(fileName));
+            var matchProvider = part switch
+            {
+                1 => CreateMatchingFunctionProviderForPart1(),
+                2 => CreateMatchingFunctionProviderForPart2()
+            };
+            
+            var count = Solve(File.ReadAllLines(fileName), matchProvider);
             Console.WriteLine($"Part {part}: {count}");
         }
 
-        private static int Solve(string[] lines)
+        private static Func<Dictionary<string, string>, Func<string, bool>> CreateMatchingFunctionProviderForPart1()
+        {
+            return rules =>
+            {
+                var targetRegex = new Regex($"^{GetRegExPatternForRuleId("0", rules)}$");
+
+                return s => targetRegex.IsMatch(s);
+            };
+        }
+        
+        // 0: 8 11 == 42+ 42 (42 (42 31) 31) 31
+        // 8: 42 | 42 8 == 42+
+        // 11: 42 31 | 42 11 31 == 42 (42 (42 31) 31) 31
+        private static Func<Dictionary<string, string>, Func<string, bool>> CreateMatchingFunctionProviderForPart2()
+        {
+            return rules =>
+            {
+                var fullRegex42 = new Regex($"^({GetRegExPatternForRuleId("42", rules)}+)");
+                var regex42 = new Regex($"{GetRegExPatternForRuleId("42", rules)}");
+                var fullRegex31 = new Regex($"^({GetRegExPatternForRuleId("31", rules)}+)");
+                var regex31 = new Regex($"{GetRegExPatternForRuleId("31", rules)}");
+
+                return s =>
+                {
+                    var matches42 = regex42.Matches(s);
+                    if (!matches42.Any())
+                        return false;
+                    
+                    var count42 = matches42.Count;
+                    var fullMatch42 = fullRegex42.Match(s);
+                    var end42 = fullMatch42.Length;
+
+                    var matches31 = regex31.Matches(s[end42..]);
+                    if (!matches31.Any())
+                        return false;
+                    
+                    var count31 = matches31.Count;
+                    var fullMatch31 = fullRegex31.Match(s[end42..]);
+                    var end31 = fullMatch31.Length;
+                    
+                    if (end42 + end31 != s.Length) return false;
+                    var res = count31 >= 1 && count42 > count31;
+
+                    if (res)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    return res;
+                };
+            };
+        }
+
+        
+        private static int Solve(string[] lines, Func<Dictionary<string, string>, Func<string, bool>> matchingProvider)
         {
             var ruleRegex = new Regex(@"(?<id>\d+):(?<rule>.+)");
 
@@ -26,10 +85,10 @@ namespace Day19
                 .TakeWhile(m => m.Success)
                 .ToDictionary(match => match.Groups["id"].Value, match => match.Groups["rule"].Value);
 
-            var targetRegex = new Regex($"^{GetRegExPatternForRuleId("0", rules)}$");
+            var createMatchFunction = matchingProvider(rules);
             return lines
                 .SkipWhile(l => ruleRegex.IsMatch(l) || l.Length == 0)
-                .Count(l => targetRegex.IsMatch(l));
+                .Count(createMatchFunction);
         }
 
         private static readonly Regex CharRegex = new("\"(?<char>\\w)\"");
