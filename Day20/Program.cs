2bafc820edc57d9c461f922bd8628b09cf7cded3
Alexey Maltsev <axel.user.perm@gmail.com>
2020-12-20 21:07:03 +0300

    Added solution for part 1 of day 20

---
+++ Day20/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day20
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var fileName = args[0];
+            var part = int.Parse(args[1]);
+
+            var result = SolvePart1(File.ReadAllText(fileName));
+            
+            Console.WriteLine($"Part {part}: {result}");
+        }
+
+        private static long SolvePart1(string text)
+        {
+            var tiles = Parse(text);
+            var adj = GetAdjacent(tiles);
+            var corners = adj.Where(pair => pair.Value.Count == 2);
+            return corners.Aggregate(1L, (mul, pair) => mul * pair.Key);
+        }
+
+        private static Dictionary<int, HashSet<int>> GetAdjacent(Tile[] tiles)
+        {
+            var adjacent = new Dictionary<int, HashSet<int>>();
+
+            foreach (var tileA in tiles)
+            {
+                adjacent[tileA.Id] = new HashSet<int>();
+                foreach (var tileB in tiles)
+                {
+                    if(tileA.Id == tileB.Id) continue;
+
+                    if (TryFindAdjacentPosition(tileA, tileB, out _))
+                    {
+                        adjacent[tileA.Id].Add(tileB.Id);
+                    }
+                }    
+            }
+
+            return adjacent;
+        }
+        
+        private enum Side
+        {
+            Left,
+            Right,
+            Top,
+            Bottom
+        }
+
+        private record AdjacentTile(Tile Tile, Side Side);
+
+        private static bool TryFindAdjacentPosition(Tile main, Tile possibleAdj, out AdjacentTile adjacent)
+        {
+            for (var rotation = 0; rotation < 4; rotation++)
+            {
+                if (CheckAdjacencyOnEachSide(main, out adjacent))
+                {
+                    return true;
+                }
+
+                if (CheckAdjacencyOnEachSide(main.FlipVertically(), out adjacent))
+                {
+                    return true;
+                }
+                
+                if (CheckAdjacencyOnEachSide(main.FlipHorizontally(), out adjacent))
+                {
+                    return true;
+                }
+
+                main = main.RotateLeft();
+            }
+
+            adjacent = default;
+            return false;
+
+            bool CheckAdjacencyOnEachSide(Tile checkedTile, out AdjacentTile adj)
+            {
+                if (checkedTile.CanPlaceTop(possibleAdj))
+                {
+                    adj = new AdjacentTile(checkedTile, Side.Top);
+                    return true;
+                }
+                
+                if (checkedTile.CanPlaceBottom(possibleAdj))
+                {
+                    adj = new AdjacentTile(checkedTile, Side.Bottom);
+                    return true;
+                }
+                
+                if (checkedTile.CanPlaceLeft(possibleAdj))
+                {
+                    adj = new AdjacentTile(checkedTile, Side.Left);
+                    return true;
+                }
+                
+                if (checkedTile.CanPlaceRight(possibleAdj))
+                {
+                    adj = new AdjacentTile(checkedTile, Side.Right);
+                    return true;
+                }
+
+                adj = default;
+                return false;
+            }
+        }
+        
+        private static Tile[] Parse(string text)
+        {
+            var tileIdRegex = new Regex(@"Tile (?<id>\d+):");
+
+            return text
+                .Split(new [] {Environment.NewLine + Environment.NewLine, "\n\n"}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Split('\n'))
+                .Select(lines =>
+                {
+                    var id = int.Parse(tileIdRegex.Match(lines[0]).Groups["id"].Value);
+                    var map = new char[lines.Length - 1, lines.Length - 1];
+                    for (var i = 0; i < map.GetLength(0); i++)
+                    {
+                        for (var j = 0; j < map.GetLength(1); j++)
+                        {
+                            map[i, j] = lines[i + 1][j];
+                        }
+                    }
+
+                    return new Tile(id, map);
+                }).ToArray();
+        }
+    }
+}
---
+++ Day20/Tile.cs
@@ -0,0 +1,110 @@
+﻿namespace Day20
+{
+    internal record Tile(int Id, char[,] Values)
+    {
+        private readonly int _n = Values.GetLength(0); 
+        
+        // a|b -> b|a
+        public Tile FlipVertically()
+        {
+            var flipped = Values.Clone() as char[,];
+
+            for (var i = 0; i < _n; i++)
+            {
+                for (var j = 0; j < _n / 2; j++)
+                {
+                    flipped[i, j] = Values[i, _n - 1 - j];
+                    flipped[i, _n - 1 - j] = Values[i, j];
+                }
+            }
+
+            return new Tile(Id, flipped);
+        }
+            
+        // a      b
+        // -  ->  -
+        // b      a
+        public Tile FlipHorizontally()
+        {
+            var flipped = Values.Clone() as char[,];
+
+            for (var i = 0; i < _n / 2; i++)
+            {
+                for (var j = 0; j < _n; j++)
+                {
+                    flipped[i, j] = Values[_n - 1 - i, j];
+                    flipped[_n - 1 - i, j] = Values[i, j];
+                }
+            }
+
+            return new Tile(Id, flipped);
+        }
+
+        
+        // Checks if other tile can be placed on top of original tile
+        public bool CanPlaceTop(Tile otherTile)
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                if (Values[0, i] != otherTile.Values[_n - 1, i])
+                    return false;
+            }
+
+            return true;
+        }
+        
+        // Checks if other tile can be placed at bottom of original tile
+        public bool CanPlaceBottom(Tile otherTile)
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                if (Values[_n - 1, i] != otherTile.Values[0, i])
+                    return false;
+            }
+
+            return true;
+        }
+        
+        // Checks if other tile can be placed left of original tile
+        public bool CanPlaceLeft(Tile otherTile)
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                if (Values[i, 0] != otherTile.Values[i, _n - 1])
+                    return false;
+            }
+
+            return true;
+        }
+        
+        // Checks if other tile can be placed right of original tile
+        public bool CanPlaceRight(Tile otherTile)
+        {
+            for (var i = 0; i < _n; i++)
+            {
+                if (Values[i, _n - 1] != otherTile.Values[i, 0])
+                    return false;
+            }
+
+            return true;
+        }
+
+        // a b c      g d a
+        // d e f  ->  h e b 
+        // g h i      i f c
+        public Tile RotateLeft()
+        {
+            var rotated = new char[_n, _n];
+
+            for (var i = 0; i < _n; ++i)
+            {
+                for (var j = 0; j < _n; ++j)
+                {
+                    rotated[i, j] = Values[_n - j - 1, i];
+                }
+            }
+
+            return new Tile(Id, rotated);
+        }
+    }
+}
